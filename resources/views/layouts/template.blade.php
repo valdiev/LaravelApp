da152c28cd32ddeeeaacfbca78d9e3896c6af31b
Valentin <valdiev@live.fr>
2021-04-22 12:07:03 +0200

    mix

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <title>Mon application</title>
-  <meta charset="utf-8">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/css/main.css" type="text/css" rel="stylesheet"/>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
