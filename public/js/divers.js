658e0ad2f087ad8f221254684880eb756a19a8b6
Valentin <valdiev@live.fr>
2021-05-03 19:54:12 +0200

    Menu connection

--- public/js/divers.js
+++ public/js/divers.js
@@ -20,4 +20,18 @@ $(document).ready(function() {
     if ( $('.user_photo').children().length == 0 ) {
         $(".no_photo").show();
    }
+
+   const $menu = $(".connexion_menu");
+   $(document).mouseup(e => {
+    if (!$menu.is(e.target) // if the target of the click isn't the container...
+    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      $menu.removeClass('show');
+   }
+  });
+
+  $(".dropdown_menu button").click(function dropDown() {
+    $(".connexion_menu").toggleClass("show");
+    })
+
 });
